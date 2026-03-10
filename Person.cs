fc399dd47f1565a0e2863b6e25d987dff01a7d11
Brendon W. <bwherry@midland.k12.ia.us>
2026-03-10 17:52:38 +0000

    gg

--- Person.cs
+++ Person.cs
@@ -1,18 +1,15 @@
 ﻿namespace knightmoves;
 
-public class Person{
-   public string FirstName;
-   public string LastName;
-   public int Age;
+public class Person
+{
+    public string FirstName;
+    public string LastName;
+    public int Age;
 
-   public Person(string firstName, string lastName, int age){
-      FirstName = firstName;
-      LastName = lastName;
-      Age = age;
-   }
-
-   public Person(string firstName, string lastName){
-      FirstName = firstName;
-      LastName = lastName;
-   }
+    public Person(string firstName, string lastName, int age = 14)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+    }
 }
